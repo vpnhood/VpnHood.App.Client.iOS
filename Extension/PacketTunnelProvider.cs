8152530695bf0fe4cac0ef80089d71b78dc885ed
Trudy <trudy@vpnhood.com>
2025-07-20 01:45:39 -0700

    Init

---
+++ Extension/PacketTunnelProvider.cs
@@ -0,0 +1,24 @@
+﻿using NetworkExtension;
+
+namespace Extension;
+
+public class PacketTunnelProvider : NEPacketTunnelProvider
+{
+    public override void StartTunnel(NSDictionary<NSString, NSObject>? options,
+        Action<NSError> startTunnelCompletionHandler)
+    {
+        // Start your VPN tunnel here
+        Console.WriteLine("Starting tunnel...");
+
+        // For now, just call completionHandler with no error to signal success
+        startTunnelCompletionHandler(null);
+    }
+
+    public override void StopTunnel(NEProviderStopReason reason, Action completionHandler)
+    {
+        Console.WriteLine("Stopping tunnel...");
+
+        // Clean up resources and stop tunnel
+        completionHandler();
+    }
+}
